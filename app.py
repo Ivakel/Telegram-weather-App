fc3bbcc50ea761b625559fec4d713b2e6ec3a2f4
Ivakel <88839357+Ivakel@users.noreply.github.com>
2022-09-18 19:13:25 +0200

    Update app.py
    laid the initial code... now later

---
+++ app.py
@@ -0,0 +1,54 @@
+import datetime as dt
+import telebot
+import requests
+import os
+from dotenv import load_dotenv
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+TELE_KEY = os.getenv("TELE_KEY")
+
+bot = telebot.TeleBot(TELE_KEY)
+
+@bot.message_handler(commands=["weather"])
+def weather(message):
+
+    CITY = message.text
+    response = weather("New York")
+    
+    data = main_weather(response)
+    compiled_data = compile(data)
+    bot.send_message(message.chat.id, compiled_data)
+
+
+
+def compile(data: dict) ->str:
+    str_data = ""
+
+    for key, value in data.items():
+        if not (type(str_data) == str):
+            value = str(value)
+        str_data += f"{key}: {value}\n"
+    return str_data
+
+def main_weather(response):
+    main_weather_info = response["main"]
+    weather = response["weather"][0]
+    description = weather["description"]
+    name = response["name"]
+
+    min_temp = round(main_weather_info["temp_min"] - 273.15)
+    max_temp = round(main_weather_info["temp_max"] - 273.15)
+
+    data = {"Name": name, "minimum_emperature": min_temp, "maximum_emperature": max_temp, "Condition": description}
+    return data
+def weather(place):
+    url = BASE_URL + "appid=" + API_KEY + "&q=" + place
+
+    response = requests.get(url).json()
+    return response
+
+bot.polling()
+    
